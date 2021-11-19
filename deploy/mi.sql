015a0a84557cca1f1506b5619d2a9a74d13d32db
Gitte Vermeiren <givermei@microsoft.com>
2021-11-19 21:36:20 +0100

    no default

--- deploy/mi.sql
+++ deploy/mi.sql
@@ -1,4 +1,3 @@
-:Setvar accountName defaultValue
 CREATE USER $(accountName) FROM EXTERNAL PROVIDER;
 ALTER ROLE db_datareader ADD MEMBER $(accountName);
 ALTER ROLE db_datawriter ADD MEMBER $(accountName);
